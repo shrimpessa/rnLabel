33da7dbe7d638d47cc9e13526bb89e3d9655eff7
Ксения <56629590+ksko-git@users.noreply.github.com>
2021-03-16 19:35:07 +0700

    Заготовка для роутов

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppNavigation } from './src/navigation/AppNavigation';
 
 export default function App() {
 
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  return <AppNavigation />
 }
---
+++ src/navigation/AppNavigation.js
@@ -0,0 +1,19 @@
+// все роуты приложения
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { MainScreen } from '../screens/MainScreen';
+import { PostScreen } from '../screens/PostScreen';
+
+const PostNavigator = createStackNavigator(
+    {
+        Main: MainScreen,
+        Post: {
+            screen: PostScreen
+        }
+    },
+    {
+        initialRouteName: 'Main'
+    }
+)
+
+export const AppNavigation = createAppContainer(PostNavigator)
